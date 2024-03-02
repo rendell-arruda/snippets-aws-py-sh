81077dc52e42e19c0f06bfb4d045b01c25946126
Rendell Arruda <rendellarruda3@gmail.com>
2024-03-02 16:39:29 -0300

    fix: comentarios nos prints

--- tests/delete_snapshot/delete_snap_7d.py
+++ tests/delete_snapshot/delete_snap_7d.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta, timezone
 def listar_snapshots_com_paginacao(regions):
     # percorre as regioes
     for region in regions:
-        print(f"### {region} ###")
+        print(f"##### {region} #####")
 
         # para cada regiao abrir uma session e um client
         session = boto3.Session(profile_name="default")
@@ -24,12 +24,8 @@ def listar_snapshots_com_paginacao(regions):
             snapshots_can_delete = []
 
             for snapshot in snapshots:
-                # # TEST
-                # print(snapshot["Description"])
-                # # TEST
 
                 # Verifica se o snap esta em uso por uma ami
-
                 response = ec2.describe_images(
                     Filters=[
                         {
@@ -41,33 +37,31 @@ def listar_snapshots_com_paginacao(regions):
 
                 if len(response["Images"]) == 0:
                     # O snapshot não está em uso por uma AMI, verifica se foi criado há mais de 7 dias
-                    data_criacao = snapshot["StartTime"].replace(
-                        tzinfo=timezone.utc
-                    )  # Modificada esta linha
+                    data_criacao = snapshot["StartTime"].replace(tzinfo=timezone.utc)
                     data_atual = datetime.now(timezone.utc)
 
                     diferenca = data_atual - data_criacao
 
-                    if diferenca.days > 1:
+                    if diferenca.days > 2:
                         # O snapshot foi criado há mais de 7 dias, pode ser excluído
-                        print(f"Excluindo snapshot {snapshot['SnapshotId']}\n")
+                        # print(f"Excluindo snapshot {snapshot['SnapshotId']}\n")
                         snapshots_can_delete.append(snapshot["SnapshotId"])
                         # ec2.delete_snapshot(SnapshotId=snapshot[ 'SnapshotId'])
                     else:
                         # O snapshot foi criado há menos de 7 dias, não pode ser excluído
-                        print(
-                            "O snapshot ",
-                            {snapshot["SnapshotId"]},
-                            "foi criado há menos de 7 dias e não pode ser excluído\n",
-                        )
+                        # print(
+                        #     "O snapshot ",
+                        #     {snapshot["SnapshotId"]},
+                        #     "foi criado há menos de 7 dias e não pode ser excluído\n",
+                        # )
                         snapshots_linked_ami.append(snapshot["SnapshotId"])
                 else:
                     # O snapshot está em uso por uma AMI, não pode ser excluído
-                    print(
-                        "O snapshot",
-                        snapshot["SnapshotId"],
-                        "está em uso por uma AMI e não pode ser excluído\n",
-                    )
+                    # print(
+                    #     "O snapshot",
+                    #     snapshot["SnapshotId"],
+                    #     "está em uso por uma AMI e não pode ser excluído\n",
+                    # )
                     snapshots_linked_ami.append(snapshot["SnapshotId"])
 
         # Imprime a lista de snapshots que não foram excluídos
