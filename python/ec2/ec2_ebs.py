0282ef88922d62bcab3ec7afffb305ef8d854a8e
Rendell Arruda <rendellarruda3@gmail.com>
2024-05-24 08:05:20 -0300

    add file temp

--- python/ec2/ec2_ebs.py
+++ python/ec2/ec2_ebs.py
@@ -1,29 +1,35 @@
 import boto3
 import csv
 
+# Define a lista de regiões para buscar as instâncias EC2
 regions = ["us-east-1"]
 
 
 def lambda_handler(event, context):
-    # criar arquivo csv
+    # Abre ou cria o arquivo CSV para escrita
     with open("./python/ec2/ec2_volumes.csv", mode="w", newline="") as csv_file:
+        # Define os nomes das colunas do CSV
         fieldnames = ["InstanceId", "VolumeId"]
+        # Cria um escritor de CSV com os nomes das colunas definidos
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        # Escreve o cabeçalho do CSV
+        # Escreve o cabeçalho no arquivo CSV
         writer.writeheader()
 
+        # Itera sobre cada região especificada
         for region in regions:
+            # Cria uma sessão boto3 usando o perfil padrão
             session = boto3.Session(profile_name="default")
+            # Cria um cliente EC2 para a região atual
             ec2_client = session.client("ec2", region_name=region)
 
-            # describe instance
+            # Descreve todas as instâncias EC2 na região
             instances = ec2_client.describe_instances()
             for reservation in instances["Reservations"]:
                 for instance in reservation["Instances"]:
-                    # listar instanceID
+                    # Obtém o ID da instância
                     instanceId = instance["InstanceId"]
 
-                    # listar volumes anexados a instancia acima
+                    # Descreve os volumes anexados à instância
                     volumes = ec2_client.describe_volumes(
                         Filters=[
                             {"Name": "attachment.instance-id", "Values": [instanceId]}
@@ -31,14 +37,17 @@ def lambda_handler(event, context):
                     )
 
                     for volume in volumes["Volumes"]:
+                        # Obtém o ID do volume
                         volumeId = volume["VolumeId"]
+                        # Imprime o ID do volume e o ID da instância no console
                         print(f" {volumeId} : {instanceId}")
 
-                        # escrever no arquivo csv
+                        # Escreve o ID da instância e o ID do volume no arquivo CSV
                         writer.writerow(
                             {"InstanceId": instanceId, "VolumeId": volumeId}
                         )
 
 
+# Ponto de entrada do script quando executado diretamente
 if __name__ == "__main__":
     lambda_handler({}, {})
